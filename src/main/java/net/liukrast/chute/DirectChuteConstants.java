82fe5d2a04940fb2c7dd3aa36595570d9f416a34
LiukRast <darthlucavader@gmail.com>
2026-01-27 23:54:32 +0100

    Remove unused LOGGER

--- src/main/java/net/liukrast/chute/DirectChuteConstants.java
+++ src/main/java/net/liukrast/chute/DirectChuteConstants.java
@@ -1,11 +1,7 @@
 package net.liukrast.chute;
 
-import com.mojang.logging.LogUtils;
-import org.slf4j.Logger;
-
 public class DirectChuteConstants {
     private DirectChuteConstants() {}
 
     public static final String MOD_ID = "direct_chute";
-    public static final Logger LOGGER = LogUtils.getLogger();
 }
